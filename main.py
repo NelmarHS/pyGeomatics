48a5296b38f6d533ab8f9765b725ef907901697e
NelmarHS <152750653+nelmarhs@users.noreply.github.com>
2024-10-14 13:14:16 +0200

    assignment 3 - Final v5 - handing in

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 
 
-lines = ["0 1 2 4", "0 9", "0 5 2"]
+class Point(object):
+    def __init__(self, xinit, yinit):
+        self.x = xinit
+        self.y = yinit
 
-for line in lines[1:]:
-    if len(line.split()) != 2:
-        continue  # Skip this iteration (invalid format of point)
+    def reflect_x(self):
+        reflected_point_x = Point(self.x, - self.y)
+        return reflected_point_x
 
-    print(line)
+# Reflected_point_x instance with object(Point(3, 5))
+reflected_point_x = Point.reflect_x(Point(3, 5))
 
+# To access a variable from within the instance, outside the instance:
+# - instance_name.instance_variable_name - i.e. reflected_point_x.x
+print(f"Reflected Point: ({reflected_point_x.x}, {reflected_point_x.y})")
--- notes.py
+++ notes.py
@@ -51,3 +51,27 @@ Class or instance variables can be changed by either:
 option 1: Change it for the whole class (all instances)
 option 2: Change it for a single instance (specific instances)
 """
+
+
+# Printing class.method results
+class Point(object):
+    def __init__(self, xinit, yinit):
+        self.x = xinit
+        self.y = yinit
+
+    def reflect_x(self):
+        reflected_point_x = Point(self.x, - self.y)
+        return reflected_point_x
+
+# Reflected_point_x instance with object(Point(3, 5))
+reflected_point_x = Point.reflect_x(Point(3, 5))
+
+# To access a variable from within the instance, outside the instance:
+# - instance_name.instance_variable_name - i.e. reflected_point_x.x
+print(f"Reflected Point: ({reflected_point_x.x}, {reflected_point_x.y})")
+
+
+
+# read txt files:
+data = open("studentdata.txt")
+lines = data.readlines()
